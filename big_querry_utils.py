63c4cbd9f06cb620d1a8717270d7dce48a10744a
gsajko <grzegorz.sajko@protonmail.com>
2023-03-13 16:03:21 +0000

    move uploading to utils

--- big_querry_utils.py
+++ big_querry_utils.py
@@ -1,7 +1,11 @@
 # %%
+import glob
 import os
+import shutil
 
-from google.cloud import bigquery
+import pandas as pd
+from google.api_core.exceptions import Conflict, PreconditionFailed
+from google.cloud import bigquery, storage
 
 # Set the absolute path to the key file
 key_file_path = "/home/sajo/key.json"
@@ -23,11 +27,8 @@ class BigQueryManager:
             dataset = self.client.create_dataset(dataset)
             # Print the dataset details
             print(f"Dataset {dataset.project}.{dataset.dataset_id} was created.")
-        except Exception as e:
-            if type(e).__name__ == "Conflict":
-                print(f"Dataset {dataset.project}.{dataset.dataset_id} already exists.")
-            else:
-                raise e
+        except Conflict:
+            print(f"Dataset {dataset.project}.{dataset.dataset_id} already exists.")
         return dataset
 
     def create_ext_table_from_parquet(self, bucket, path, table_name):
@@ -59,3 +60,70 @@ class BigQueryManager:
         for table_name in table_names:
             assert table_name in existing_table_names
             print(f"Table {table_name} created successfully")
+
+
+class GcsUploader:
+    def __init__(self, project_dir, bucket_name, location_region):
+        self.project_dir = project_dir
+        self.bucket_name = bucket_name
+        self.location_region = location_region
+
+    def create_bucket(self):
+        try:
+            storage_client = storage.Client()
+            bucket = storage_client.bucket(self.bucket_name)
+            bucket = storage_client.create_bucket(bucket, location=self.location_region)
+            print("Bucket {} created".format(bucket.name))
+        except Conflict:
+            print("Bucket {} already exists".format(self.bucket_name))
+
+    def upload_blob(self, source_file_name, destination_blob_name):
+        """Uploads a file to the bucket."""
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(self.bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        generation_match_precondition = 0
+        blob.upload_from_filename(
+            source_file_name, if_generation_match=generation_match_precondition
+        )
+        print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+    def save_to_gcs(self, src_folder, file, dest_folder):
+        try:
+            # Set the absolute path to the source file
+            src_file_path = os.path.join(src_folder, file)
+            try:
+                df = pd.read_csv(src_file_path)
+            except UnicodeDecodeError:
+                df = pd.read_csv(src_file_path, encoding="Windows-1252")
+            # save to tmp folder
+            file_name = file.split(".")[0]
+            # lower column names
+            df.columns = [x.lower() for x in df.columns]
+            # set type of date columns
+            df["transactiondateutc"] = pd.to_datetime(df["transactiondateutc"])
+            
+            dest_file_name = f"{file_name}.parquet"
+            dest_file_path = os.path.join(self.project_dir, "tmp", dest_file_name)
+            df.to_parquet(dest_file_path)
+            # upload to GCS
+            dest_blob_name = f"{dest_folder}/{dest_file_name}"
+            self.upload_blob(dest_file_path, dest_blob_name)
+            # remove tmp file
+            os.remove(dest_file_path)
+        except PreconditionFailed:
+            print(f"File {file} already exists in {dest_folder} folder")
+
+    def upload_from_folders(self, folders):
+        os.makedirs(os.path.join(self.project_dir, "tmp"), exist_ok=True)
+        for folder in folders:
+            src_folder = os.path.join(self.project_dir, "data/data", folder)
+            os.chdir(src_folder)
+            extension = "csv"
+            all_filenames = [i for i in glob.glob("*.{}".format(extension))]
+            # create tmp folder
+            for file in all_filenames:
+                print(f"uploading: {file}")
+                self.save_to_gcs(src_folder=src_folder, file=file, dest_folder=folder)
+        # Remove all files and subdirectories inside tmp directory
+        shutil.rmtree(os.path.join(self.project_dir, "tmp"))
--- ingestion.py
+++ ingestion.py
@@ -1,10 +1,22 @@
 # %%|
-from big_querry_utils import BigQueryManager
+from big_querry_utils import BigQueryManager, GcsUploader
 
 # %%
-bq_manager = BigQueryManager(dataset_id="fuel_qld", location="EU")
+project_dir = "/home/sajo/fuel_prices_qld/"
+dataset_name = "230313fuel_qld"
+bucket_name = "230313_fuelprice3"
+location_region = "EU"
+# %%
+# upload data from repo to GCS bucket
+uploader = GcsUploader(project_dir, bucket_name, location_region)
+uploader.create_bucket()
+uploader.upload_from_folders(["week", "month"])
+
+# %%
+# Create external tables
+bq_manager = BigQueryManager(dataset_id=dataset_name, location=location_region)
 ext_tables = [("external_fuel_week", "week"), ("external_fuel_month", "month")]
-bq_manager.create_new_ext_tables(bucket_name="230311fire", new_ext_tables=ext_tables)
+bq_manager.create_new_ext_tables(bucket_name=bucket_name, new_ext_tables=ext_tables)
 bq_manager.check_tables_exist([table_name for table_name, _ in ext_tables])
 
 
